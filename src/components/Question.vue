f749e764906eabef28bf2920cd4194a5672bc107
Evan2Lin <107451083+Evan2Lin@users.noreply.github.com>
2022-08-26 17:25:16 +0800

    Overwrite descriptions

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -9,7 +9,7 @@
                 <v-img id="img" @click="retournerImage(choixQuestion[1])" class="mx-auto ml-sm-0" :src="require('@/assets/img/' + choixQuestion[1] + '.png')" width="300" height="200"/>
             </v-col>
 
-			<v-col cols="12" class="mt-5"><strong>Select the design that is most correct</strong></v-col>
+			<v-col cols="12" class="mt-5"><strong>Choose an user-interface design that is more common in most websites and apps</strong></v-col>
 		</v-row>
     </div>
 </template>
